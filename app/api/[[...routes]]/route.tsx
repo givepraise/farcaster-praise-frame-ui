f16f5e574fe0ed63d0fc8375983cada75cace433
Ramin <raminramazanpour@gmail.com>
2024-08-22 13:22:38 +0330

    add attestation smart contract

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -15,59 +15,79 @@ const app = new Frog({
   title: 'Praise in Farcaster',
 })
 
-// Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 app.frame('/', (c) => {
     return c.res({
         action: '/finish',
         image: (
-            <div style={{ color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 60 }}>
-                Perform a transaction
+            <div
+                style={{
+                    alignItems: 'center',
+                    background: 'linear-gradient(to right, #432889, #17101F)',
+                    backgroundSize: '100% 100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexWrap: 'nowrap',
+                    height: '100%',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                }}
+            >
+                <div
+                    style={{
+                        color: 'white',
+                        fontSize: 60,
+                        fontStyle: 'normal',
+                        letterSpacing: '-0.025em',
+                        lineHeight: 1.4,
+                        marginTop: 30,
+                        padding: '0 120px',
+                        whiteSpace: 'pre-wrap',
+                    }}
+                >
+                    Perform an attestation!
+                </div>
             </div>
         ),
         intents: [
-            <TextInput placeholder="Value (ETH)" />,
-            <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
-            // <Button.Transaction target="/mint">Mint</Button.Transaction>,
+            <Button.Transaction target="/attest">Attest</Button.Transaction>,
         ]
     })
 })
 
 app.frame('/finish', (c) => {
-    const { transactionId } = c
+    const {transactionId} = c
     return c.res({
         image: (
-            <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+            <div style={{color: 'white', display: 'flex', fontSize: 60}}>
                 Transaction ID: {transactionId}
             </div>
         )
     })
 })
 
-app.transaction('/send-ether', (c) => {
-    const { inputText } = c
-    // Send transaction response.
-    return c.send({
-        chainId: 'eip155:10',
-        to: '0x91eBbA819E4BbA03065a106290afcB44deB1F9d6',
-        value: parseEther(inputText!),
-    })
-})
-
-app.transaction('/mint', (c) => {
-    const { inputText } = c
-    // Contract transaction response.
+app.transaction('/attest', (c) => {
     return c.contract({
         abi,
         chainId: 'eip155:10',
-        functionName: 'mint',
-        args: [69420n],
-        to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-        value: parseEther(inputText!)
+        functionName: 'attest',
+        args: [{
+            schema: '0xa76299ae6a66b66ff48344f36c0fa657a0a9eeb6721248311df9cf25748e4405',
+            data: {
+                revocable: true,
+                recipient: '0x4236864edA3B7863B4E543c2661FC14029e5fBEC',
+                refUID: '0x0000000000000000000000000000000000000000000000000000000000000000',
+                expirationTime: 0n,
+                data: '0x0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000572616d696e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008676976657468696f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010666f72206372656174696e672065746800000000000000000000000000000000',
+                value: 0n,
+            },
+        }],
+        to: '0x4200000000000000000000000000000000000021',
     })
 })
 
-devtools(app, { serveStatic })
+devtools(app, {serveStatic})
 
 export const GET = handle(app)
 export const POST = handle(app)
