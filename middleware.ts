e4cc80a58959d1d4a7c4b0e3feb91374343d52ce
Ramin <raminramazanpour@gmail.com>
2024-09-13 02:51:10 +0330

    redirect main bot url to givepraise site

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,25 @@ import { NextResponse, NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     console.log('------------- ')
-    console.log(request.headers)
+    console.log(request)
     console.log('------------- ')
-    // return NextResponse.redirect(new URL('/home', request.url))
+    const bots = [
+        'TwitterBot',
+        'python-requests',
+        'aiohttp',
+        'Go-http-client',
+        'axios',
+        'facebookexternalhit',
+        'node-fetch',
+        'FCBot',
+        'probe-image-size']
+    const userAgent = request.headers.get('user-agent') || ''
+    const isBot = bots.some(bot => userAgent.toLowerCase().includes(bot.toLowerCase()))
+    const referer = request.headers.get('referer')
+    const isOpenedOnBrowser = referer && referer.indexOf('warpcast') > -1
+    if (!isBot && isOpenedOnBrowser) {
+        return NextResponse.redirect('https://givepraise.xyz/')
+    }
     return NextResponse.next();
 }
 
